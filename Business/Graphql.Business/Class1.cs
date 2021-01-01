bd879462831dadb43e82109bf611231371d1a6dd
Vlad Levchenko <kosras.96@gmail.com>
2021-01-01 21:25:21 +0200

    Add project files.

---
+++ Business/Graphql.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Graphql.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ Data/Graphql.Domain/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GraphqlDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphqlDomain
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+               : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = default!;
+    }
+}
